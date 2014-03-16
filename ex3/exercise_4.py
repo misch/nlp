9b9a1462676184eda5f7979884763ddd3f505f3c
Michele Wyss <michele_wyss@students.unibe.ch>
2014-03-16 19:17:59 +0100

    Finished ex4

--- ex3/exercise_4.py
+++ ex3/exercise_4.py
@@ -8,29 +8,23 @@ initial_split = word_expression.findall(f.read())
 f.close()
 
 words = []
+
+# filter out the "<...>"-words
 for word in initial_split:
     if not (word[0]=="<" and word[len(word)-1] == ">"):
         words.append(word)
 
-bigrams = []
 count = collections.Counter()
 for i in range(0,len(words)-1):
     bigram = tuple(words[i:i+2])
-    bigrams.append(bigram)
     count[bigram] += 1
 
-for lala, freq in count.most_common(len(count)):
-    if freq > 200:
-        print(str(lala)+ ": "+str(freq))
+print("Please enter a threshold:")
+threshold = input()
 
-#print(len(bigrams_unique))
-
-
-#print(initial_split[0:100])
-#print(words[0:15])
-#print(words[15:30])
-#print(words[30:45])
-#print(words[45:60])
-#print(words[60:75])
-#print(words[75:90])
-#print(words[90:105])
+outfile = open('ouput_ex4.txt','w')
+outfile.write("The following bigrams occur more than " + str(threshold) + " times: \n\n")
+for bigram, frequency in count.most_common(len(count)):
+    if frequency > threshold:
+        outfile.write(str(frequency)+ ": \t\t"+str(bigram) + "\n")
+outfile.close()
