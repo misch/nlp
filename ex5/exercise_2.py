cf5609b3b59ba297569e4edf41ed36e3036a023d
Michèle Wyss <michele_wyss@students.unibe.ch>
2014-03-30 18:24:19 +0200

    Dictionary implemented.

--- ex5/exercise_2.py
+++ ex5/exercise_2.py
@@ -2,26 +2,55 @@
 
 from exercise_1 import fnv
 
+
 class Dictionary:
     def __init__(self, initial_capacity):
         self.capacity = initial_capacity
-        self.hashes = []
+        self.hash_table = [[] for _ in range(initial_capacity)] # generate a list of empty lists with the give capacity
 
     def insert(self, string):
-        self.hashes.append(fnv(string))
-        #TODO: throw exception if hash already exists
+        position = self.get_key(string)
+        hash_values = self.hash_table[position]  # List of strings that are have been mapped to the same hash key.
+        if string in hash_values:
+            raise Exception('The string {} already exists in the dictionary.'.format(string))
+
+        hash_values.append(string)
+
+        if self.size() > self.capacity/2:
+            self.capacity *= 2
+            old_hash_table = self.hash_table
+            self.hash_table = [[] for _ in range(self.capacity)]
+            for hash_values in old_hash_table:
+                for s in hash_values:
+                    self.insert(s)
+
 
     def remove(self, string):
-        self.hashes.remove(fnv(string))
-        #TODO: throw exception if hash doesn't exist
+        position = self.get_key(string)
+        hash_values = self.hash_table[position]
+
+        if not string in hash_values:
+            raise Exception("The string {} is not in the dictionary".format(string))
+        hash_values.remove(string)
+
 
     def lookUp(self, string):
-        #TODO
-        return string
+        position = self.get_key(string)
+        hash_values = self.hash_table[position]
+
+        if string in hash_values:
+            return string
+        return None
 
     def size(self):
-        #TODO
-        return 0
+        size = 0
+        for x in self.hash_table:
+            if x:
+                size += 1
+        return size
 
     def capacity(self):
         return self.capacity
+
+    def get_key(self, string):
+        return (fnv(string) % self.capacity)
