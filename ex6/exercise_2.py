e7d2ea53d8a0b311890a6fa059281bc37a78532e
Michèle Wyss <michele_wyss@students.unibe.ch>
2014-04-06 21:48:08 +0200

    exercise 2

---
+++ ex6/exercise_2.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+import re
+
+f = open('output_ex1.txt')
+expression = re.compile('^[a-zA-Z]{4}$')
+alphabet = [chr(let) for let in range(ord('a'),ord('z')+1)]
+
+initial_dict = set()
+for word in f:
+    initial_dict.add(word.strip())
+f.close()
+
+f = open('english_dictionary.txt')
+english_dict = set()
+for line in f:
+    matches = expression.findall(line)
+    for word in matches:
+        english_dict.add(word.strip())
+f.close()
+
+new_dict = set(initial_dict)
+
+for word in initial_dict:
+    for idx, character in enumerate(word):
+        for letter in alphabet:
+            new_word = list(word)
+            new_word[idx] = letter
+            new_word = "".join(new_word)
+            if new_word in english_dict:
+                new_dict.add(new_word)
+
+out = open('output_ex2.txt','w')
+
+for word in new_dict:
+    out.write(word+"\n")
+out.close()
