afaad1cd746a9908802f4fbe68bd590ba22a8227
Michèle Wyss <michele_wyss@students.unibe.ch>
2014-05-19 22:05:47 +0200

    Solved ex.1

---
+++ ex10/ex1_py.py
@@ -0,0 +1,50 @@
+#usr/bin/python
+
+import numpy as np
+
+def doStuff(A1, A2, A3, Q):
+    m_string = ';'.join([' '.join(map(str,x)) for x in [A1, A2, A3, Q]])
+    m = np.matrix(m_string, dtype=np.float)
+    m_rel = np.transpose(m/m.sum(axis=1))
+
+    mean = np.mean(m_rel[:,0:3],1)
+    std = np.std(m_rel[:,0:3],1)
+
+    centered = m_rel - np.kron([1, 1, 1, 1], mean)
+    z_score = np.divide(centered, np.kron([1, 1, 1, 1], std))
+    #print(np.kron([1, 1, 1, 1], mean))
+    #print(m_rel)
+    delta1 = np.abs(z_score[:,0]- z_score[:,3])
+    delta2 = np.abs(z_score[:,1]- z_score[:,3])
+    delta3 = np.abs(z_score[:,2]- z_score[:,3])
+
+    n = 5
+
+    delta1 = sum(delta1)/n
+    delta2 = sum(delta2)/n
+    delta3 = sum(delta3)/n
+
+    print(delta1)
+    print(delta2)
+    print(delta3)
+    #print(m_rel[:,1]-mean)
+    #print(m_rel[:,2]-mean)
+    #print(m_rel[:,3]-mean)
+
+def doA():
+    A1 = [20, 36, 90, 75, 12]
+    A2 = [21, 100, 100, 3, 57]
+    A3 = [3, 23, 12, 67, 34]
+    Q = [25, 40, 45, 10, 20]
+    doStuff(A1,A2,A3,Q)
+
+
+def doB():
+    A1 = [2,78,10,11,5]
+    A2 = [36,45,30,32,29]
+    A3 = [100,5,6,23,0]
+    Q = [3,2,5,4,2]
+
+    doStuff(A1,A2,A3,Q)
+
+doB()
